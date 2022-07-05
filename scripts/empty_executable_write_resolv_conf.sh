09b2cd3077c4ccf18147326617ee7a663176add1
Iosif Takakura <takakura.yusuke@gmail.com>
2022-07-06 08:29:12 +0900

    Update scripts/empty_executable_write_resolv_conf.sh

--- scripts/empty_executable_write_resolv_conf.sh
+++ scripts/empty_executable_write_resolv_conf.sh
@@ -7,8 +7,9 @@ fi
 touch $TMP_FILE_NAME
 
 # sh のバッチファイル実行上から powershell が見えなかったので、絶対パス指定
-POWERSHELL_COMMAND="/mnt/c/Windows/System32/WindowsPowerShell/v1.0//powershell.exe"
-NAMESERVER=$($POWERSHELL_COMMAND ipconfig | grep "IPv4" | tail -1 | awk '{print $NF}' | awk 'sub(/\r$/,"")')
+#POWERSHELL_COMMAND="/mnt/c/Windows/System32/WindowsPowerShell/v1.0//powershell.exe"
+#NAMESERVER=$($POWERSHELL_COMMAND ipconfig | grep "IPv4" | tail -1 | awk '{print $NF}' | awk 'sub(/\r$/,"")')
+NAMESERVER=$@
 
 echo "# created by script, $(date "+%Y/%m/%d %H:%M:%S")" >> $TMP_FILE_NAME
 echo "nameserver $NAMESERVER" >> $TMP_FILE_NAME
