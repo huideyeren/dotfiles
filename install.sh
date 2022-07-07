26ec7634eb0fa4599bb90e5b671067caa84fde08
Iosif Takakura <takakura.yusuke@gmail.com>
2022-07-08 06:53:16 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,9 @@
 
 sudo apt update && sudo apt install -y zsh
 
+git clone https://github.com/romkatv/zsh-defer.git ~/zsh-defer
+
+curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh \
+    | bash -s -- --repo rossmacarthur/sheldon --to ~/.local/bin
+
 sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply huideyeren
