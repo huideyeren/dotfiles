df532e5f635ab3c9677b6eb4d00179cebe2525a2
Iosif Takakura <takakura.yusuke@gmail.com>
2022-07-08 14:58:18 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
-sudo apt update && sudo apt install -y zsh git
-
+# zsh-defer
 git clone https://github.com/romkatv/zsh-defer.git ~/zsh-defer
+
+# golang
+if [[ -f /etc/os-release ]]; then
+  source /usr/lib/os-release
+  case $ID in
+    debian ) sudo apt update && sudo apt install -y golang;;
+    ubuntu ) sudo apt update && sudo apt install -y golang;;
+    fedora ) sudo dnf -y install golang;;
+    centos ) sudo dnf -y install golang;;
+    opensuse ) sudo udo zypper --non-interactive install golang;;
+    alpine ) sudo apk install golang;;
+  esac
+else
+    echo "/etc/os-release is not exist."
+fi
+
+
+# chezmoi
 
 sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply huideyeren
