a35712c3d27e5b4f05231dacb7351c0f5a69a341
Iosif Takakura <takakura.yusuke@gmail.com>
2022-07-09 18:21:16 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 if [[ -f /etc/os-release ]]; then
   source /usr/lib/os-release
   case $ID in
-    debian ) sudo apt update && sudo apt install -y zsh;;
-    ubuntu ) sudo apt update && sudo apt install -y zsh;;
+    debian ) sudo apt update && sudo apt install -y zsh && sudo apt install -y build-essential libffi-dev libssl-dev zlib1g-dev liblzma-dev libbz2-dev libreadline-dev libsqlite3-dev libopencv-dev tk-dev git;;
+    ubuntu ) sudo apt update && sudo apt install -y zsh && sudo apt install -y build-essential libffi-dev libssl-dev zlib1g-dev liblzma-dev libbz2-dev libreadline-dev libsqlite3-dev libopencv-dev tk-dev git;;
     fedora ) sudo dnf update && sudo dnf group install -y "Development Tools" && sudo dnf install -y wget tar zsh && sudo dnf install -y gcc zlib-devel bzip2 bzip2-devel readline readline-devel sqlite sqlite-devel openssl openssl-devel git libffi-devel tar make;;
     centos ) sudo dnf update && sudo dnf group install -y "Development Tools" && sudo dnf install -y wget tar zsh && sudo dnf install -y gcc zlib-devel bzip2 bzip2-devel readline readline-devel sqlite sqlite-devel openssl openssl-devel git libffi-devel tar make;;
     miraclelinux ) sudo dnf update && sudo dnf group install -y "Development Tools" && sudo dnf install -y wget tar zsh && sudo dnf install -y gcc zlib-devel bzip2 bzip2-devel readline readline-devel sqlite sqlite-devel openssl openssl-devel git libffi-devel tar make;;
-    opensuse ) sudo udo zypper --non-interactive install zsh;;
-    alpine ) sudo apk install git && sudo apk install zsh && sudo apk install tar && sudo apk install wget;;
+    opensuse ) sudo udo zypper --non-interactive install zsh && sudo zypper --non-interactive install install gcc automake bzip2 libbz2-devel xz xz-devel openssl-devel ncurses-devel readline-devel zlib-devel tk-devel libffi-devel sqlite3-devel;;
+    alpine ) sudo apk install --no-cache git bash build-base libffi-dev openssl-dev bzip2-dev zlib-dev xz-dev readline-dev sqlite-dev tk-dev zsh tar wget;;
   esac
 fi
 
