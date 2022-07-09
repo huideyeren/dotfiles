e39b322b0f1cf9cb117b65325b9d2bf65ae7848f
Iosif Takakura <takakura.yusuke@gmail.com>
2022-07-09 20:25:39 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ if [[ -f /etc/os-release ]]; then
     centos ) sudo dnf update && sudo dnf group install -y "Development Tools" && sudo dnf install -y wget tar zsh && sudo dnf install -y gcc zlib-devel bzip2 bzip2-devel readline readline-devel sqlite sqlite-devel openssl openssl-devel git libffi-devel tar make;;
     miraclelinux ) sudo dnf update && sudo dnf group install -y "Development Tools" && sudo dnf install -y wget tar zsh && sudo dnf install -y gcc zlib-devel bzip2 bzip2-devel readline readline-devel sqlite sqlite-devel openssl openssl-devel git libffi-devel tar make;;
     opensuse ) sudo udo zypper --non-interactive install zsh && sudo zypper --non-interactive install install gcc automake bzip2 libbz2-devel xz xz-devel openssl-devel ncurses-devel readline-devel zlib-devel tk-devel libffi-devel sqlite3-devel go go-doc go-vim subversion mercurial mercurial-lang git;;
+    opensuse ) sudo udo zypper --non-interactive install zsh && sudo zypper --non-interactive install install gcc automake bzip2 libbz2-devel xz xz-devel openssl-devel ncurses-devel readline-devel zlib-devel tk-devel libffi-devel sqlite3-devel go go-doc go-vim subversion mercurial mercurial-lang git;;
     alpine ) apk add --update-cache --no-cache git bash build-base libffi-dev openssl-dev bzip2-dev zlib-dev xz-dev readline-dev sqlite-dev tk-dev zsh tar wget;;
   esac
 fi
